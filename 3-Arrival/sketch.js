0d0665123be4210c2dc8e2335f18e0cb11b0ed47
micbuffa <micbuffa@gmail.com>
2025-11-10 17:59:05 +0100

    ajout de correction exercices sur pursue evade

--- 3-Arrival/sketch.js
+++ 3-Arrival/sketch.js
@@ -1,8 +1,9 @@
-const nbVehicules = 20;
+let nbVehicules = 20;
 let target;
 let vehicle;
 let vehicles = [];
 let font, points = [];
+let mode = "Snake";
 
 // Appelée avant de démarrer l'animation
 function preload() {
@@ -11,21 +12,24 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
 
   // On crée un véhicule à la position (100, 100)
   //vehicle = new Vehicle(100, 100);
 
-  // on creer des vehicules
-  for (let i = 0; i < nbVehicules; i++) {
-    vehicles.push(new Vehicle(random(width), random(height)));
-  }
 
   target = createVector(random(width), random(height));
 
   // le texte
   // Get the point array.
-   points = font.textToPoints('Rabat', 6, 250, 320, { sampleFactor:  0.04 });
+  points = font.textToPoints('Rabat', 116, 250, 320, { sampleFactor: 0.04 });
+
+  // autant de véhicules que de points sur le texte  
+  nbVehicules = points.length;
+  // on creer des vehicules
+  for (let i = 0; i < nbVehicules; i++) {
+    vehicles.push(new Vehicle(random(width), random(height)));
+  }
 
 
 }
@@ -38,7 +42,6 @@ function draw() {
   //background(0, 0, 0, 10);
 
   // Dessin des points du texte
-// Draw a dot at each point.
   for (let p of points) {
     push();
     noFill();
@@ -61,27 +64,40 @@ function draw() {
     // si on a affaire au premier véhicule
     // alors il suit la souris (target)
     let steeringForce;
-    if (index === 0) {
-      steeringForce = vehicle.arrive(target);
-    } else {
-      // j'ai affaire à un autre véhicule, pas le premier.
-      // je veux qu'il suive le précédent
-      let vehiculePrecedent = vehicles[index - 1];
 
-      // le véhicule courant fait un arrive sur le précédent
-      // le second parametre est la distance par rapport au centre du
-      // véhicule précédent. Doit être inférieure à rayonDistanceFreinage
-      steeringForce = vehicle.arrive(vehiculePrecedent.pos, 20);
+    switch (mode) {
+      case "Snake":
+        if (index === 0) {
+          steeringForce = vehicle.arrive(target);
+        } else {
+          // j'ai affaire à un autre véhicule, pas le premier.
+          // je veux qu'il suive le précédent
+          let vehiculePrecedent = vehicles[index - 1];
 
-      // On dessine une ligne entre le véhicule courant et le véhicule précédent
-      push();
-      noFill();
-      stroke(random(255), 255, 255, 60);
-      strokeWeight(20);
-      line(vehicle.pos.x, vehicle.pos.y, 
-           vehiculePrecedent.pos.x, vehiculePrecedent.pos.y)
-      pop();
+          // le véhicule courant fait un arrive sur le précédent
+          // le second parametre est la distance par rapport au centre du
+          // véhicule précédent. Doit être inférieure à rayonDistanceFreinage
+          steeringForce = vehicle.arrive(vehiculePrecedent.pos, 20);
+
+          // On dessine une ligne entre le véhicule courant et le véhicule précédent
+          push();
+          noFill();
+          stroke(random(255), 255, 255, 60);
+          strokeWeight(20);
+          line(vehicle.pos.x, vehicle.pos.y,
+            vehiculePrecedent.pos.x, vehiculePrecedent.pos.y)
+          pop();
+        }
+        break;
+      case "Texte":
+        // Les vaisseaux vont chacun suivre leur cible
+
+        // le véhicule courant a pour cible points[index]
+        pointTarget = createVector(points[index].x, points[index].y);
+        steeringForce = vehicle.arrive(pointTarget);
+        break;
     }
+
 
     vehicle.applyForce(steeringForce);
     vehicle.update();
@@ -93,5 +109,9 @@ function draw() {
 function keyPressed() {
   if (key === 'd') {
     Vehicle.debug = !Vehicle.debug;
+  } else if (key === "s") {
+    mode = "Snake";
+  } else if (key === "t") {
+    mode = "Texte";
   }
 }
